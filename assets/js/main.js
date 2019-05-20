a93cea49f478bc55e1cab59befc279b2023256c0
Ryan Walker <rywalker@overstock.com>
2019-05-20 09:22:13 -0600

    Minimized all photos, added relevant clients along with client page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 (function($) {
-	const css = 'text-shadow: -1px -1px hsl(125, 100%, 20%), 1px 1px hsl(125, 100%, 25%), 3px 2px hsl(125, 100%, 30%), 5px 3px hsl(125, 100%, 35%), 7px 4px hsl(125, 100%, 40%), 9px 5px hsl(125, 100%, 45%), 11px 6px hsl(125, 100%, 50%); font-size: 50px; color: rgb(203, 23, 47);';
+	const css = 'text-shadow: -1px -1px hsl(125, 100%, 20%), 1px 1px hsl(125, 100%, 25%), 3px 2px hsl(125, 100%, 30%), 5px 3px hsl(125, 100%, 35%), 7px 4px hsl(125, 100%, 40%), 9px 5px hsl(125, 100%, 45%), 11px 6px hsl(125, 100%, 50%); font-size: 50px; color: #24b2ff;';
 	console.log('%c SM&D ', css);
 
 	skel.breakpoints({
